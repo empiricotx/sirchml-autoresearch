aec0c4259651b6c8815ff3d3ea764c7ef21795d4
Lucas Platter <lucas.platter@empiricotx.com>
2026-03-12 20:55:25 -0700

    Try 0.02 dropout under scaled metrics

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ ARCHITECTURE = ArchitectureSpec(
     family="mlp",
     hidden_dims=(320, 160, 80, 40),
     activation="silu",
-    dropout=0.05,
+    dropout=0.02,
     normalization="none",
     use_bias=True,
 )
