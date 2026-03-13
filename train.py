d76e95743cc06045168a235b2a545dc416448259
Lucas Platter <lucas.platter@empiricotx.com>
2026-03-12 20:26:44 -0700

    Restore last keep after discard 384-192-96

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from prepare import ArchitectureContext, ArchitectureSpec, run_experiment
 
 ARCHITECTURE = ArchitectureSpec(
     family="mlp",
-    hidden_dims=(384, 192, 96),
+    hidden_dims=(256, 128, 64),
     activation="gelu",
     dropout=0.0,
     normalization="none",
